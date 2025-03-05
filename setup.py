8d1e25c8f22edc7aa5bdcde2310fdd2cf255d308
mirandi1 <86965658+mirandi1@users.noreply.github.com>
2025-03-05 12:36:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyRheo',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),  # Automatically discovers all packages and sub-packages
     install_requires=[
         'scipy',
